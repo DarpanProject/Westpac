bfb271c98c558c6e721d7228c1a94daabfa8727c
darpan19 <darpan19@192.168.0.5>
2019-05-07 23:45:52 +0530

    Test Instructions/Set Up

---
+++ NewProject/src/TestScripts/TestInstructionsNotes.java
@@ -0,0 +1,25 @@
+package TestScripts;
+
+public class TestInstructionsNotes {
+
+/*.  Hi , This is just for reference 
+ * 
+ * 
+ * Instructions and prerequisites / Setup Information for the test 
+ * 
+ *     Step 1:  To execute the test , we can use automation tool Selenium web drier
+ *     Step 2   We need to have all required jar files installed for selenium to execute the test	
+ *     Step 3:  We can use TestNG framework to write the test cases
+ *     Step 4:  We need to have TestNG jar files also in the eclipse
+ *     Step 5 :  We should have one repository created in the Git to upload all the files.
+ *     
+ *     
+ *          THANK YOU !!!!  
+ */
+	
+	
+}
+	
+	
+	
+
